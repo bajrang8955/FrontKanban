545a95925064b81c573fd6b735e3e530fff16818
Anh Phương <trananhphuong83@gmail.com>
2017-12-04 12:35:41 +0700

    Update update.sql

--- sql/update.sql
+++ sql/update.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE IF NOT EXISTS `0_projects` (
-    `proj_id` varchar(11) NOT NULL,
+    `proj_id` int(11) NOT NULL AUTO_INCREMENT,
     `proj_name` varchar(100) NOT NULL,
     `proj_description` varchar(100) DEFAULT NULL,
     `proj_type` INT(11) NOT NULL DEFAULT 0,
@@ -9,5 +9,6 @@ CREATE TABLE IF NOT EXISTS `0_projects` (
     `begin_date` date DEFAULT NULL,
     `end_date` date DEFAULT NULL,
     `owner_id` int(11) NOT NULL,
+    `closed` tinyint(1) NOT NULL DEFAULT 0,
     PRIMARY KEY (`proj_id`)
 ) ENGINE=InnoDB;
